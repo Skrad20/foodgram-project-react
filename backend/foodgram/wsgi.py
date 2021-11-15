9fe1c731142e1fb4997192089c37348a17c78c80
Skrad20 <skrad200052@yandex.ru>
2021-11-15 21:52:54 +0300

    workflow

--- backend/foodgram/wsgi.py
+++ backend/foodgram/wsgi.py
@@ -12,7 +12,8 @@ import sys
 
 from django.core.wsgi import get_wsgi_application
 
-sys.path.append('./backend/foodgram')
+sys.path.append('/backend/foodgram')
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'foodgram.settings')
 
 application = get_wsgi_application()
