4f5b9b04195d7a58d29d8dba2de625a5362332a7
Skrad20 <skrad200052@yandex.ru>
2021-12-05 19:41:10 +0300

    1.0.23 -Изменения в странице пользователяэ

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '&-*r16^^ltxrpb2cbdhph7i=1abush7)-dxj2q&((g7ys2t28a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['51.250.21.224', 'localhost', 'backend', '127.0.0.1', 'web']
 
@@ -112,7 +112,7 @@ WSGI_APPLICATION = 'foodgram.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 
-if True:
+if False:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
