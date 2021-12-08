3067660b64952d854c22e9feb3d87140a9cc7115
Skrad20 <skrad200052@yandex.ru>
2021-12-08 14:10:05 +0300

    1.1.2 - revu 2

--- backend/recipes/serializers.py
+++ backend/recipes/serializers.py
@@ -1,8 +1,8 @@
 from django.shortcuts import get_object_or_404
 from drf_extra_fields.fields import Base64ImageField
 from rest_framework import serializers
-from users.serializers import UserSerializer
 
+from users.serializers import UserSerializer
 from .models import (Favorite, IngredAmount, Ingredient, Recipe, ShoppingCart,
                      Tag)
 
