a4e7abceb9ff83e2d8b91664c551c353de2129a3
Skrad20 <skrad200052@yandex.ru>
2021-12-12 18:48:04 +0300

    1.1.7 -review

--- backend/recipes/validators.py
+++ backend/recipes/validators.py
@@ -10,18 +10,18 @@ class CustomRecipeValidator:
         ingredients = data.get('ingredients')
         set_ingredients = set()
         if not ingredients:
-            raise ValidationError(
+            return ValidationError(
                 'Нужно добавить хотя бы один ингредиент.'
             )
         else:
             for ingredient in ingredients:
                 if int(ingredient.get('amount')) <= 0:
-                    raise ValidationError(
+                    return ValidationError(
                         ('Значение количества не может быть меньше единицы.')
                     )
                 ingredient_id = ingredient.get('id')
                 if ingredient_id in set_ingredients:
-                    raise ValidationError(
+                    return ValidationError(
                         'Ингрединеты не должны повторяться'
                     )
                 set_ingredients.add(ingredient_id)
@@ -29,19 +29,19 @@ class CustomRecipeValidator:
 
         tags = data.get('tags')
         if not tags:
-            raise ValidationError(
+            return ValidationError(
                 'Нужно добавить хотя бы один тэг.'
             )
         elif tags:
             if Tag.objects.filter(id__in=tags).count() < len(tags):
-                raise ValidationError(
+                return ValidationError(
                     'Такого тэга нет в базе.'
                 )
         data['tags'] = tags
 
         cooking_time = data.get('cooking_time')
         if int(cooking_time) < 1:
-            raise ValidationError(
+            return ValidationError(
                 'Время приготовления должно быть больше нуля.'
             )
         data['cooking_time'] = cooking_time
